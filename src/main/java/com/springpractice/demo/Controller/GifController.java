30db36ef0bad2aeb19c960a0c494d6a9072b3b45
Cerissa Antill <cerissa.antill@gmail.com>
2019-05-04 07:59:07 -0500

    added GifController

---
+++ src/main/java/com/springpractice/demo/Controller/GifController.java
@@ -0,0 +1,16 @@
+package com.springpractice.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class GifController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String listGifs() {
+        return "List of all the GIFs!";
+
+    }
+}
