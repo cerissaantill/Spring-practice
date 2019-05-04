c5708f917200396226ce92e4648a6b905288634f
Cerissa Antill <cerissa.antill@gmail.com>
2019-05-04 07:36:49 -0500

    added Config file

---
+++ src/main/java/com/springpractice/demo/Config.java
@@ -0,0 +1,17 @@
+package com.springpractice.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+
+@EnableAutoConfiguration
+public class Config {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Config.class, args);
+
+
+
+    }  // main method
+
+}  // Config class
